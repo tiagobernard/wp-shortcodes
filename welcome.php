b687230a49f5dddc283d9fb7ee6b6b8e5c29f540
Tiago Bernardes <tiago@tiagobernardes.com.br>
2021-04-08 13:23:57 -0300

    Create welcome.php
    Adicione uma mensagem de boas vindas em uma loja virtual

---
+++ welcome.php
@@ -0,0 +1,16 @@
+// MENSAGEM DE BOAS VINDAS - Insira até a linha 13 no arquivo functions.php do tema
+// é possível usar outros além do display_name... user_firstname, user_lastname, user_login, user_nicename, user_email
+	function show_loggedin_function( $atts ) {
+	global $current_user, $user_login;
+		  get_currentuserinfo();
+	add_filter('widget_text', 'do_shortcode');
+	if ($user_login) 
+	return 'Olá ' . $current_user->display_name . '!' . '<a style="text-decoration:none; background-color:#531869; color:#fff; padding:2px 3px;cursor:pointer;" href="' . wp_logout_url() . '">Sair</a>'; //personalize os estilos de acordo com o projeto
+	else
+	return 'Olá visitante! ' . '<a style="text-decoration:none; background-color:#531869; color:#fff; padding:0 5px;cursor:pointer;" href="' . site_url() . '/minha-conta/">Criar conta/Entrar</a>'; //personalize os estilos de acordo com o projeto
+	
+	}
+	add_shortcode( 'welcome_lab82', 'show_loggedin_function' );
+
+//adicionar esse shortcode onde quer que a mensagem de boas vindas apareça
+[welcome_lab82]
